8eae18b50aeb56835c2c97b5c0b084c2dd018f54
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-09-11 18:04:07 +0200

    Add Repository. Add Logging

--- Purefolio-backend/DatabaseContext.cs
+++ Purefolio-backend/DatabaseContext.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Purefolio.DatabaseContext;
+using Purefolio_backend.Models;
 
 namespace Purefolio.DatabaseContext
 {
@@ -18,45 +18,15 @@ namespace Purefolio.DatabaseContext
             // TODO: Use a configuration file instead of hard coding database string.
             .UseNpgsql("Host=localhost;Port=10101;Database=purefolio;Username=purefolio;Password=password")
             .UseSnakeCaseNamingConvention();
-    }
 
-    // TODO: Move out to seperate file
-    public class Nace
-    {
-        public int NaceId { get; set; }
-        [Required]
-        public string NaceCode { get; set; } // Primary key
-    }
-    public class Region
-    {
-        public int RegionId { get; set; }
-        [Required]
-        public string RegionCode { get; set; } // Primary key
-    }
-    public class RegionData
-    {
-        public int RegionDataId { get; set; }
-        public int RegionDetailId { get; set; }
-        public string RegionEmission { get; set; } // Foreign key
-
-    }
-
-
-    public class NaceRegionData
-    {
-        public int NaceRegionDataId{ get; set; } // Primary key
-        [Required]
-        public int NaceId { get; set; } // Foreign key. Primary key
-        [Required]
-        public Nace Nace { get; set; }
-        [Required]
-        public int RegionId { get; set; } // Foreign key. Primary key
-        [Required]
-        public Region Region { get; set; } // Nacigation property
-        public int year { get; set; }
-        public string emissionPerYer { get; set; }
-        public string genderPayGap { get; set; }
-        public string corruptionRate { get; set; }
+        // This initialises database with tables on startup 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Region>().ToTable("Region");
+            modelBuilder.Entity<Nace>().ToTable("Nace");
+            modelBuilder.Entity<RegionData>().ToTable("RegionData");
+            modelBuilder.Entity<NaceRegionData>().ToTable("NaceRegionData");
+        }
     }
 
 
