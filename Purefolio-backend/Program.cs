b7904c874c5cbdcabce77b59514fe00ec681e958
Sindre Sivertsen <sindrejohan1@gmail.com>
2020-09-11 18:04:07 +0200

    Add Repository. Add Logging

--- Purefolio-backend/Program.cs
+++ Purefolio-backend/Program.cs
@@ -1,72 +1,66 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Purefolio.DatabaseContext;
 using Purefolio_backend.Models;
 
 namespace Purefolio_backend
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            // Main application configurer
-            var host =
-                CreateHostBuilder(args)
-                    .ConfigureLogging((hostingContext, logging) =>
-                    {
-                        logging
-                            .AddConfiguration(hostingContext
-                                .Configuration
-                                .GetSection("Logging"));
-                        logging.AddConsole();
-                        logging.AddDebug();
-                        logging.AddEventSourceLogger();
-                    })
-                    .Build();
+      // Main application configurer
+      var host =
+        CreateHostBuilder(args)
+          .ConfigureLogging((hostingContext, logging) =>
+          {
+            logging
+              .AddConfiguration(hostingContext
+                .Configuration
+                .GetSection("Logging"));
+            logging.AddConsole();
+            logging.AddDebug();
+            logging.AddEventSourceLogger();
+          })
+          .Build();
 
-            // Logger for startup logging
-            var logger =
-                host.Services.GetRequiredService<ILogger<Repository>>();
+      // Logger for startup logging
+      var logger = host.Services.GetRequiredService<ILogger<Repository>>();
 
-            SeedDatabase(logger);
+      SeedDatabase (logger);
 
-            host.Run();
-        }
-
-        // TODO: Move to seperate location
-        private static void SeedDatabase(ILogger<Repository> logger)
-        {
-            // TODO: Rewrite this to a proper seed method
-            try
-            {
-                var rp = new Repository(logger);
-                var response =
-                    rp
-                        .AddOrUpdateNace(new Nace()
-                        { NaceCode = "NACE!", NaceId = 1 });
-                logger.LogWarning(response.ToString());
-                var allNace = rp.getAllNace();
-                logger.LogWarning(allNace.ToString());
-            }
-            catch (Exception exception)
-            {
-                logger.LogError(exception.StackTrace);
-            }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host
-                .CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      host.Run();
     }
+
+    // TODO: Move to seperate location
+    private static void SeedDatabase(ILogger<Repository> logger)
+    {
+      // TODO: Rewrite this to a proper seed method
+      try
+      {
+        logger.LogWarning("TEEEEST");
+        var rp = new Repository(logger);
+        var response =
+          rp.AddOrUpdateNace(new Nace() { NaceCode = "NACE!", NaceId = 1 });
+        logger.LogWarning(response.ToString());
+        var allNace = rp.getAllNace();
+        logger.LogWarning(allNace.ToString());
+      }
+      catch (Exception exception)
+      {
+        logger.LogError(exception.StackTrace);
+      }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
+  }
 }
--- Purefolio-backend/Startup.cs
+++ Purefolio-backend/Startup.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,44 +8,44 @@ using Purefolio.DatabaseContext;
 
 namespace Purefolio_backend
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddDbContext<DatabaseContext>();
-            services.AddSingleton<Repository>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(
-            IApplicationBuilder app,
-            IWebHostEnvironment env,
-            ILoggerFactory loggerFactory
-        )
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddDbContext<DatabaseContext>();
+      services.AddSingleton<Repository>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(
+      IApplicationBuilder app,
+      IWebHostEnvironment env,
+      ILoggerFactory loggerFactory
+    )
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app
+        .UseEndpoints(endpoints =>
+        {
+          endpoints.MapControllers();
+        });
+    }
+  }
 }
