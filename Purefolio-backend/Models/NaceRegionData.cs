142798e3491d75f89047dc6bed27988ee9421154
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-09-25 09:18:51 +0200

    Add getHashCode and fix linting

--- Purefolio-backend/Models/NaceRegionData.cs
+++ Purefolio-backend/Models/NaceRegionData.cs
@@ -6,43 +6,48 @@ namespace Purefolio_backend.Models
 {
     public class NaceRegionData
   {
-   public int NaceRegionDataId { get; set; } // Primary key
-        [Required]
-        public int NaceId { get; set; } // Foreign key. Primary key
-        [Required]
-        public Nace Nace { get; set; }
-        [Required]
-        public int RegionId { get; set; } // Foreign key. Primary key
-        [Required]
-        public Region Region { get; set; } // Navigation property
-        public int year { get; set; }
-        public double emissionPerYer { get; set; }
-        public double genderPayGap { get; set; }
+    public int NaceRegionDataId { get; set; } // Primary key
+    [Required]
+    public int NaceId { get; set; } // Foreign key. Primary key
+    [Required]
+    public Nace Nace { get; set; }
+    [Required]
+    public int RegionId { get; set; } // Foreign key. Primary key
+    [Required]
+    public Region Region { get; set; } // Navigation property
+    public int year { get; set; }
+    public double emissionPerYer { get; set; }
+    public double genderPayGap { get; set; }
+
+    public override int GetHashCode()
+    {
+      return base.GetHashCode();
+    }
 
     public override bool Equals(object obj) 
+    { 
+        if (obj == null || GetType() != obj.GetType()) 
         { 
-            if (obj == null || GetType() != obj.GetType()) 
-            { 
-                return false; 
-            } 
-            NaceRegionData other = (NaceRegionData) obj; 
-            return this.NaceId == other.NaceId && this.RegionId == other.RegionId && this.year == other.year; 
+            return false; 
         } 
+        NaceRegionData other = (NaceRegionData) obj; 
+        return this.NaceId == other.NaceId && this.RegionId == other.RegionId && this.year == other.year; 
+    } 
  
-        public NaceRegionData merge(NaceRegionData other) 
+    public NaceRegionData merge(NaceRegionData other) 
+    { 
+      if(this.Equals(other)){ 
+        List<System.Reflection.PropertyInfo> props = this.GetType().GetProperties().ToList(); 
+        foreach (System.Reflection.PropertyInfo prop in props) 
         { 
-          if(this.Equals(other)){ 
-            List<System.Reflection.PropertyInfo> props = this.GetType().GetProperties().ToList(); 
-            foreach (System.Reflection.PropertyInfo prop in props) 
+            object value = prop.GetValue(other); 
+            if(value != null) 
             { 
-                object value = prop.GetValue(other); 
-                if(value != null) 
-                { 
-                  prop.SetValue(this, value); 
-                } 
+              prop.SetValue(this, value); 
             } 
-          } 
-          return this; 
         } 
-    }
+      } 
+      return this; 
+    } 
+  }
 }
