64f5f8e193a7a7e56ce3f204516b13ff0eddfc02
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-10-01 15:03:15 +0200

    Add attribute tests for RegionData and NaceRegionData

--- Purefolio-backendTests/Models/RegionDataTests.cs
+++ Purefolio-backendTests/Models/RegionDataTests.cs
@@ -1,14 +1,49 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq; 
 
 namespace Purefolio_backend.Models.Tests
 {
     [TestClass()]
     public class RegionDataTests
     {
-        
+        [TestMethod()]
+        public void ObjectsShouldHaveRightAmountOfAttributes()
+        {
+            int testRegionId = 1;
+            int testYear = 2020;
+
+            RegionData nrd = new RegionData(){regionId = testRegionId, year = testYear};
+            
+            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
+
+            Assert.AreEqual(props.Count(),7); 
+        }
 
         [TestMethod()]
-        public void TestMerge()
+        public void AttributesShouldBeNullable()
+        {
+            int testRegionId = 1;
+            int testYear = 2020;
+
+            RegionData nrd = new RegionData(){regionId = testRegionId, year = testYear};
+            
+            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
+
+            List<string> requiredFields = new List<string>(){"regionDataId", "region", "regionId", "year"};
+
+            foreach (System.Reflection.PropertyInfo prop in props)
+            {
+                if(!requiredFields.Contains(prop.Name))
+                {
+                    Assert.AreEqual(prop.GetValue(nrd), null);
+                }
+                
+            }
+        }
+        
+        [TestMethod()]
+        public void ObjectsShouldMergeProperties()
         {
             int testRegionId = 0;
             int testYear = 2020;
