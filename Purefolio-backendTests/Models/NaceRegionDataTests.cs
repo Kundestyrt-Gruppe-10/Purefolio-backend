b5d44203c550b502f5b14785cec66872c7cdc1aa
Bjørn Are Therkelsen <37076083+batherk@users.noreply.github.com>
2020-10-02 14:48:53 +0200

    Test merging and equality of NaceRegionData objects and RegionData objects (#16)
    * Add setup method, add tests for equalilty check and merging
    
    * Remove unused RegionData reference in test
    
    * Add setup method and add tests for merging and equality check

--- Purefolio-backendTests/Models/NaceRegionDataTests.cs
+++ Purefolio-backendTests/Models/NaceRegionDataTests.cs
@@ -8,63 +8,129 @@ namespace Purefolio_backend.Models.Tests
     public class NaceRegionDataTests
     {
 
+        private NaceRegionData nrd1WithEmissionPerYear;
+        private NaceRegionData nrd1WithGenderPayGap;
+        private NaceRegionData nrd1WithEmissionAndGenderPayGap;
+        private NaceRegionData nrd2WithoutData;
+        private List<string> requiredFields;
+
+        private int regionId1 = 1;
+        private int regionId2 = 2;
+
+        private int naceId1 = 1;
+        private int testYear = 2020;
+        private int testGenderPayGap = 4;
+        private int testEmissionPerYer = 80000000;
+
+         
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            nrd1WithEmissionPerYear = new NaceRegionData(){naceId = naceId1, regionId = regionId1, year=testYear, emissionPerYer = testEmissionPerYer };
+            nrd1WithGenderPayGap = new NaceRegionData(){naceId = naceId1,regionId = regionId1, year=testYear, genderPayGap = testGenderPayGap };
+            nrd1WithEmissionAndGenderPayGap = new NaceRegionData(){naceId = naceId1, regionId = regionId1, year=testYear, genderPayGap = testGenderPayGap,emissionPerYer = testEmissionPerYer};
+            nrd2WithoutData = new NaceRegionData(){regionId = regionId2, year = testYear};
+            requiredFields = new List<string>(){"naceRegionDataId", "region", "regionId", "nace", "naceId", "year"};
+        }
+        
         [TestMethod()]
         public void ObjectsShouldHaveRightAmountOfAttributes()
         {
-            int testRegionId = 1;
-            int testNaceId = 1;
-            int testYear = 2020;
-
-             NaceRegionData nrd = new NaceRegionData(){regionId = testRegionId, naceId = testNaceId, year = testYear};
-            
-            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
-            List<System.Reflection.PropertyInfo> expected = new List<System.Reflection.PropertyInfo>();
-
-            List<string> requiredFields = new List<string>(){"naceRegionDataId", "region", "regionId", "nace", "naceId", "year"};
-
+            List<System.Reflection.PropertyInfo> props = nrd2WithoutData.GetType().GetProperties().ToList(); 
             Assert.AreEqual(props.Count(),8); 
         }
 
         [TestMethod()]
         public void AttributesShouldBeNullable()
         {
-            int testRegionId = 1;
-            int testNaceId = 1;
-            int testYear = 2020;
-
-             NaceRegionData nrd = new NaceRegionData(){regionId = testRegionId, naceId = testNaceId, year = testYear};
             
-            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
-            List<System.Reflection.PropertyInfo> expected = new List<System.Reflection.PropertyInfo>();
-
-            List<string> requiredFields = new List<string>(){"naceRegionDataId", "region", "regionId", "nace", "naceId", "year"};
+           List<System.Reflection.PropertyInfo> props = nrd2WithoutData.GetType().GetProperties().ToList();
 
             foreach (System.Reflection.PropertyInfo prop in props)
             {
                 if(!requiredFields.Contains(prop.Name))
                 {
-                    Assert.AreEqual(prop.GetValue(nrd), null);
+                    Assert.AreEqual(prop.GetValue(nrd2WithoutData), null);
                 }
                 
             }
         }
-
+        
         [TestMethod()]
         public void ObjectsShouldMergeProperties()
         {
-            int testRegionId = 0;
-            int testYear = 2020;
-            int testEmissionPerYear = 100;
-            int testGenderPayGap = 100;
+            NaceRegionData rdMerged = nrd1WithGenderPayGap.merge(nrd1WithEmissionPerYear);
+            Assert.AreEqual(nrd1WithEmissionAndGenderPayGap, rdMerged);
+        }
 
+        [TestMethod()]
+        public void ObjectsShouldBeEqualWhenSameIdsAndYears()
+        {
+            Assert.AreEqual(nrd1WithGenderPayGap,nrd1WithEmissionPerYear);
+        }
 
-            NaceRegionData nrd1 = new NaceRegionData(){regionId = testRegionId, year=testYear, emissionPerYer = testEmissionPerYear};
-            NaceRegionData nrd2 = new NaceRegionData(){regionId = testRegionId, year=testYear, genderPayGap = testGenderPayGap};
-            NaceRegionData nrdCombined = new NaceRegionData(){regionId = testRegionId, year=testYear, emissionPerYer = testEmissionPerYear, genderPayGap = testGenderPayGap};
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotBeEqualWhenDifferentRegionIds(int id1, int id2)
+        {
+            NaceRegionData rd1 = new NaceRegionData(){regionId = id1, naceId = naceId1, year=testYear };
+            NaceRegionData rd2 = new NaceRegionData(){regionId = id2, naceId = naceId1, year=testYear };  
+            Assert.AreNotEqual(rd1,rd2);
+        }
 
-            NaceRegionData nrdMerged = nrd1.merge(nrd2);
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotBeEqualWhenDifferentNaceIds(int id1, int id2)
+        {
+            NaceRegionData rd1 = new NaceRegionData(){naceId = id1, regionId = regionId1, year=testYear };
+            NaceRegionData rd2 = new NaceRegionData(){naceId = id2, regionId = regionId1, year=testYear };  
+            Assert.AreNotEqual(rd1,rd2);
+        }
+
+        [TestMethod()]
+        [DataRow(2020,2019)]
+        public void ObjectsShouldNotBeEqualWhenDifferentYears(int year1, int year2)
+        {
+            NaceRegionData nrd1 = new NaceRegionData(){year = year1, regionId = regionId1, naceId = naceId1 };
+            NaceRegionData nrd2 = new NaceRegionData(){year = year2, regionId = regionId2, naceId = naceId1 };
+            Assert.AreNotEqual(nrd1,nrd2);
+        }
+
+        [TestMethod()]
+        [DataRow(2020,2019)]
+        public void ObjectsShouldNotMergeWhenDifferentYears(int year1, int year2)
+        {
+            NaceRegionData nrd1 = new NaceRegionData(){year=year1, regionId = regionId1, naceId = naceId1 };
+            NaceRegionData nrd2 = new NaceRegionData(){year=year2, regionId = regionId2, naceId = naceId1, emissionPerYer = testEmissionPerYer};
             
-            Assert.AreEqual(nrdMerged, nrdCombined);
+            NaceRegionData nrdMerged = nrd1.merge(nrd2);
+
+            Assert.AreEqual(nrd1,nrdMerged);
+        }
+
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotMergeWhenDifferentRegionIds(int id1, int id2)
+        {
+            NaceRegionData nrd1 = new NaceRegionData(){regionId = id1, naceId = naceId1, year=testYear };
+            NaceRegionData nrd2 = new NaceRegionData(){regionId = id2, naceId = naceId1, year=testYear, emissionPerYer = testEmissionPerYer};
+            
+            NaceRegionData nrdMerged = nrd1.merge(nrd2);
+
+            Assert.AreEqual(nrd1,nrdMerged);
+        }
+
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotMergeWhenDifferentNaceIds(int id1, int id2)
+        {
+            NaceRegionData nrd1 = new NaceRegionData(){naceId = id1, regionId = regionId1,  year=testYear };
+            NaceRegionData nrd2 = new NaceRegionData(){naceId = id2, regionId = regionId1,  year=testYear, emissionPerYer = testEmissionPerYer};
+            
+            NaceRegionData nrdMerged = nrd1.merge(nrd2);
+
+            Assert.AreEqual(nrd1,nrdMerged);
         }
     }
 }
--- Purefolio-backendTests/Models/RegionDataTests.cs
+++ Purefolio-backendTests/Models/RegionDataTests.cs
@@ -7,36 +7,49 @@ namespace Purefolio_backend.Models.Tests
     [TestClass()]
     public class RegionDataTests
     {
+
+        private RegionData rd1WithCorruptionData;
+        private RegionData rd1WithPopulationData;
+        private RegionData rd1WithCorruptionAndPopulationData;
+        private RegionData rd2WithoutData;
+        private List<string> requiredFields;
+
+        private int regionId1 = 1;
+        private int regionId2 = 2;
+        private int testYear = 2020;
+        private int testCorruptionRate = 80;
+        private int testPopulation = 80000000;
+
+         
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            rd1WithCorruptionData = new RegionData(){regionId = regionId1, year=testYear, corruptionRate = testCorruptionRate};
+            rd1WithPopulationData = new RegionData(){regionId = regionId1, year=testYear, population = testPopulation};
+            rd1WithCorruptionAndPopulationData = new RegionData(){regionId = regionId1, year=testYear, corruptionRate = testCorruptionRate, population = testPopulation};
+            rd2WithoutData = new RegionData(){regionId = regionId2, year = testYear};
+            requiredFields = new List<string>(){"regionDataId", "region", "regionId", "year"};
+        }
+        
         [TestMethod()]
         public void ObjectsShouldHaveRightAmountOfAttributes()
         {
-            int testRegionId = 1;
-            int testYear = 2020;
-
-            RegionData nrd = new RegionData(){regionId = testRegionId, year = testYear};
-            
-            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
-
+            List<System.Reflection.PropertyInfo> props = rd2WithoutData.GetType().GetProperties().ToList(); 
             Assert.AreEqual(props.Count(),7); 
         }
 
         [TestMethod()]
         public void AttributesShouldBeNullable()
         {
-            int testRegionId = 1;
-            int testYear = 2020;
-
-            RegionData nrd = new RegionData(){regionId = testRegionId, year = testYear};
             
-            List<System.Reflection.PropertyInfo> props = nrd.GetType().GetProperties().ToList(); 
-
-            List<string> requiredFields = new List<string>(){"regionDataId", "region", "regionId", "year"};
+           List<System.Reflection.PropertyInfo> props = rd2WithoutData.GetType().GetProperties().ToList();
 
             foreach (System.Reflection.PropertyInfo prop in props)
             {
                 if(!requiredFields.Contains(prop.Name))
                 {
-                    Assert.AreEqual(prop.GetValue(nrd), null);
+                    Assert.AreEqual(prop.GetValue(rd2WithoutData), null);
                 }
                 
             }
@@ -45,18 +58,56 @@ namespace Purefolio_backend.Models.Tests
         [TestMethod()]
         public void ObjectsShouldMergeProperties()
         {
-            int testRegionId = 0;
-            int testYear = 2020;
-            int testCorruptionRate = 80;
-            int testPopulation = 80000000;
+            RegionData rdMerged = rd1WithCorruptionData.merge(rd1WithPopulationData);
+            Assert.AreEqual(rd1WithCorruptionAndPopulationData, rdMerged);
+        }
 
-            RegionData rd1 = new RegionData(){regionId = testRegionId, year=testYear, corruptionRate = testCorruptionRate};
-            RegionData rd2 = new RegionData(){regionId = testRegionId, year=testYear, population = testPopulation};
-            RegionData rdCombined = new RegionData(){regionId = testRegionId, year=testYear, corruptionRate = testCorruptionRate, population = testPopulation};
+        [TestMethod()]
+        public void ObjectsShouldBeEqualWhenSameIdsAndYears()
+        {
+            Assert.AreEqual(rd1WithCorruptionData,rd1WithPopulationData);
+        }
 
-            RegionData rdMerged = rd1.merge(rd2);
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotBeEqualWhenDifferentIds(int id1, int id2)
+        {
+            RegionData rd1 = new RegionData(){regionId = id1, year=testYear };
+            RegionData rd2 = new RegionData(){regionId = id2, year=testYear };  
+            Assert.AreNotEqual(rd1,rd2);
+        }
+
+        [TestMethod()]
+        [DataRow(2020,2019)]
+        public void ObjectsShouldNotBeEqualWhenDifferentYears(int year1, int year2)
+        {
+            RegionData rd1 = new RegionData(){regionId = regionId1, year=year1 };
+            RegionData rd2 = new RegionData(){regionId = regionId2, year=year2 };
+            Assert.AreNotEqual(rd1,rd2);
+        }
+
+        [TestMethod()]
+        [DataRow(2020,2019)]
+        public void ObjectsShouldNotMergeWhenDifferentYears(int year1, int year2)
+        {
+            RegionData rd1 = new RegionData(){regionId = regionId1, year=year1 };
+            RegionData rd2 = new RegionData(){regionId = regionId2, year=year2, corruptionRate = 80};
             
-            Assert.AreEqual(rdCombined, rdMerged);
+            RegionData rdMerged = rd1.merge(rd2);
+
+            Assert.AreEqual(rd1,rdMerged);
+        }
+
+        [TestMethod()]
+        [DataRow(1,2)]
+        public void ObjectsShouldNotMergeWhenDifferentIds(int id1, int id2)
+        {
+            RegionData rd1 = new RegionData(){regionId = id1, year=testYear };
+            RegionData rd2 = new RegionData(){regionId = id2, year=testYear, corruptionRate = 80};
+            
+            RegionData rdMerged = rd1.merge(rd2);
+
+            Assert.AreEqual(rd1,rdMerged);
         }
     }
 }
