15c825a9e518f4fe5d5ab83760ae1dbcbafa4696
Mathea <mathea.hildre@gmail.com>
2020-10-06 11:56:48 +0200

    Refactor JSONConverter

--- Purefolio-backend/Services/JSONConverter.cs
+++ Purefolio-backend/Services/JSONConverter.cs
@@ -22,6 +22,7 @@ namespace Purefolio_backend
             this._logger = _logger;
         }
         
+
         public List<NaceRegionData> convert(String jsonString, String dataset){
             List<NaceRegionData> nrdList = new List<NaceRegionData>();
             List<string> IDinOrder = new List<string>();
@@ -36,39 +37,15 @@ namespace Purefolio_backend
             var value = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonDict["value"].ToString());
             var id = JsonConvert.DeserializeObject<List<string>>(jsonDict["id"].ToString());
             var size = JsonConvert.DeserializeObject<List<int>>(jsonDict["size"].ToString());
-            var dimension = JsonConvert.DeserializeObject<Dictionary<string, Object>>(jsonDict["dimension"].ToString());
-            var nace_r2_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(dimension["nace_r2"].ToString());
-            nace_r2_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(nace_r2_complex["category"].ToString());
-            var nace_r2 = JsonConvert.DeserializeObject<Dictionary<string, int>>(nace_r2_complex["index"].ToString());
-            var inverted_nace_r2 = nace_r2.ToDictionary(x => x.Value, x => x.Key);
-            var geo_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(dimension["geo"].ToString());
-            geo_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(geo_complex["category"].ToString());
-            var geo = JsonConvert.DeserializeObject<Dictionary<string, int>>(geo_complex["index"].ToString());
-            var inverted_geo = geo.ToDictionary(x => x.Value, x => x.Key);
-            var time_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(dimension["time"].ToString());
-            time_complex = JsonConvert.DeserializeObject<Dictionary<string, Object>>(time_complex["category"].ToString());
-            var time = JsonConvert.DeserializeObject<Dictionary<string, int>>(time_complex["index"].ToString());
-            var inverted_time = time.ToDictionary(x => x.Value, x => x.Key);
+            var nace = getNestedField("nace_r2", jsonDict);
+            var geo = getNestedField("geo", jsonDict);
+            var time = getNestedField("time", jsonDict);
 
+            CheckValidDataset(id, size);
 
-            //checks for valid dataset (not more than 1 element in categories except for nace, geo, and time)
-            // TODO: Actually handle when datasets are wrong.
+            (IDinOrder, SizeinOrder) = MakeOrderedLists(id, size);
 
-            for (int i = 0; i < id.Count; i++)
-            {   
-                if(id[i].Equals("nace_r2") || id[i].Equals("geo") || id[i].Equals("time"))
-                {
-                    IDinOrder.Add(id[i]);
-                    SizeinOrder.Add(size[i]);
-                }
-                
-                else{
-                    if(!size[i].Equals(1)){
-                        _logger.LogError(message:"Dataset invalid. " + id[i] + " did not have exactly 1 field.");
-                    }
-                }
-            }
-
+        
             
             List<int> indexes = new List<int>();
             foreach (KeyValuePair<string, string> entry in value)
@@ -81,9 +58,9 @@ namespace Purefolio_backend
                 indexes.Add((indexOfData % (SizeinOrder[1]*SizeinOrder[2]) / SizeinOrder[2]));
                 indexes.Add((indexOfData % (SizeinOrder[1]*SizeinOrder[2])) % SizeinOrder[2]);
 
-                string Nace = inverted_nace_r2[indexes[IDinOrder.IndexOf("nace_r2")]];
-                string Geo = inverted_geo[indexes[IDinOrder.IndexOf("geo")]];
-                string Year = inverted_time[indexes[IDinOrder.IndexOf("time")]];
+                string Nace = nace[indexes[IDinOrder.IndexOf("nace_r2")]];
+                string Geo = geo[indexes[IDinOrder.IndexOf("geo")]];
+                string Year = time[indexes[IDinOrder.IndexOf("time")]];
 
                 _logger.LogInformation(message:"Value on index " + entry.Key +  ": " + entry.Value + ", Nace: " + Nace + ", Geo: " + Geo + ", Year: " + Year);
             }
@@ -91,6 +68,42 @@ namespace Purefolio_backend
             return nrdList;
         }
 
+
+
+        private void CheckValidDataset(List<string> id, List<int> size){
+            //checks for valid dataset (not more than 1 element in categories except for nace, geo, and time)
+            // TODO: Actually handle when datasets are wrong.
+
+            for (int i = 0; i < id.Count; i++)
+            {   
+                if (!(id[i].Equals("nace_r2") || id[i].Equals("geo") || id[i].Equals("time")) && !size[i].Equals(1)){
+                        _logger.LogError(message:"Dataset invalid. " + id[i] + " did not have exactly 1 field.");
+                }
+            }
+        }
+
+        private (List<string>, List<int>) MakeOrderedLists(List<string> id, List<int> size){
+            List<string> IDinOrder = new List<string>();
+            List<int> SizeinOrder = new List<int>();
+
+            for (int i = 0; i < id.Count; i++)
+            {   
+                if(id[i].Equals("nace_r2") || id[i].Equals("geo") || id[i].Equals("time"))
+                {
+                    IDinOrder.Add(id[i]);
+                    SizeinOrder.Add(size[i]);
+                }
+            }
+            return (IDinOrder, SizeinOrder);
+        }
+
+        private Dictionary<int, string> getNestedField(string field_name, Dictionary<string, Object> jsonDict){
+            var dimension = JsonConvert.DeserializeObject<Dictionary<string, Object>>(jsonDict["dimension"].ToString());
+            var temp_field = JsonConvert.DeserializeObject<Dictionary<string, Object>>(dimension[field_name].ToString());
+            temp_field = JsonConvert.DeserializeObject<Dictionary<string, Object>>(temp_field["category"].ToString());
+            var field = JsonConvert.DeserializeObject<Dictionary<string, int>>(temp_field["index"].ToString());
+            return field.ToDictionary(x => x.Value, x => x.Key);
+        }
         
     }
 }
