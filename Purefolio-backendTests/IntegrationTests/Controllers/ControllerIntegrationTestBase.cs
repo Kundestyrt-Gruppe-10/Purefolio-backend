478afeda93b6d460b39ff3876e9d69a0c508dc04
Bjørn Are Therkelsen <37076083+batherk@users.noreply.github.com>
2020-10-14 11:23:23 +0200

    Add test database for integration testing (#28)
    * Add TestBase
    
    * Add test base with test context
    
    * Add controller test base and fix tests for nace controller and nrd controller
    
    * Move files to integration folder and rename classes accordingly

---
+++ Purefolio-backendTests/IntegrationTests/Controllers/ControllerIntegrationTestBase.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Purefolio_backend.Controllers;
+using Purefolio_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Purefolio_backend.Controllers.Tests
+{
+    [TestClass()]
+    public class ControllerIntegrationTestBase : IntegrationTestBase
+    {
+        protected DatabaseStore _databaseStore;
+        
+        public ControllerIntegrationTestBase() : base()
+        {
+            var databaseStoreLogger = new Mock<ILogger<DatabaseStore>>();
+            _databaseStore = new DatabaseStore(databaseStoreLogger.Object, this._context);
+        }
+        
+    }
+}
---
+++ Purefolio-backendTests/IntegrationTests/Controllers/NaceRegionDataControllerTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Purefolio_backend.Controllers;
+using Purefolio_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Purefolio_backend.Controllers.Tests
+{
+    [TestClass()]
+    public class NaceRegionDataControllerTests : ControllerIntegrationTestBase
+    {
+        public NaceRegionDataController naceRegionDataController;
+        public NaceRegionDataControllerTests() : base()
+        {
+            var controllerLogger = new Mock<ILogger<NaceRegionDataController>>();
+            naceRegionDataController = new NaceRegionDataController(controllerLogger.Object, this._databaseStore);
+        }
+
+        [TestMethod()]
+        [DataRow(1,15,2018)]
+        [DataRow(1,15,2019)]
+        public void GetTest(int naceId, int regionId, int year)
+        {            
+            Assert.IsTrue(this._databaseStore.getNaceRegionData(naceId, regionId, year).SequenceEqual(naceRegionDataController.Get(naceId, regionId, year)));
+        }
+    }
+}
