9e51acfc11f3379cbe107fb81a1c132b34ed0ff6
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-10-15 11:24:09 +0200

    Add esg-factor endpoint

---
+++ Purefolio-backend/Controllers/EuroStatTableController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Purefolio_backend.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Purefolio_backend.Controllers
+{
+  [ApiController]
+  [Route("/tables")]
+  public class EuroStatTableController : ControllerBase
+  {
+    private readonly ILogger<EuroStatTableController> _logger;
+
+    private DatabaseStore _databaseStore;
+
+    public EuroStatTableController(
+      ILogger<EuroStatTableController> logger, DatabaseStore databaseStore
+    )
+    {
+      _logger = logger;
+      _databaseStore = databaseStore;
+    }
+
+        [HttpGet]
+        [Route("esg-factors")]
+        public List<string> getESGFactors()
+        {
+            return _databaseStore.getAllEuroStatTables().ConvertAll((EuroStatTable table) => table.attributeName);
+        }
+    }
+  }
