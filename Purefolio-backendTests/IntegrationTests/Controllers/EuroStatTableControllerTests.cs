3927f24ed239a9ac190ca4965ace3735ca0759b9
Bjørn Are Therkelsen <37076083+batherk@users.noreply.github.com>
2020-10-15 12:58:11 +0200

    Add esg factor endpoint (#34)
    * Add esg-factor endpoint
    
    * Add test for EuroStatController
    
    * Make esg-factor test run and add tables in test database

---
+++ Purefolio-backendTests/IntegrationTests/Controllers/EuroStatTableControllerTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Purefolio_backend.Controllers;
+using Purefolio_backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Purefolio_backend.Controllers.Tests
+{
+    [TestClass()]
+    public class EuroStatTableControllerTests : ControllerIntegrationTestBase
+    {
+        public EuroStatTableController euroStatTableController;
+        public EuroStatTableControllerTests() : base()
+        {
+            var controllerLogger = new Mock<ILogger<EuroStatTableController>>();
+            euroStatTableController = new EuroStatTableController(controllerLogger.Object, this._databaseStore);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {   List<string> expected =  new List<string>(){
+                "emissionPerYear",
+                "workAccidentsIncidentRate",
+                "genderPayGap",
+                "environmentTaxes",
+                "fatalAccidentsAtWork",
+                "temporaryemployment",
+                "employeesPrimaryEducation",
+                "employeesSecondaryEducation",
+                "employeesTertiaryEducation"
+            };
+            Assert.IsTrue(expected.SequenceEqual(euroStatTableController.getESGFactors()));
+        }
+    }
+}
