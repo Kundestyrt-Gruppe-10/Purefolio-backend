42e6a37003cf8f761c6f5d9b840380a8305c5b1e
Mathea <mathea.hildre@gmail.com>
2020-11-12 18:07:35 +0100

    Add dataset CO2

--- Purefolio-backend/Migrations/20201112163555_V16_Add_CO2_dataset.Designer.cs
+++ Purefolio-backend/Migrations/20201112163555_V16_Add_CO2_dataset.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Purefolio.DatabaseContext;
@@ -9,9 +10,10 @@ using Purefolio.DatabaseContext;
 namespace Purefolio_backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20201112163555_V16_Add_CO2_dataset")]
+    partial class V16_Add_CO2_dataset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Purefolio-backend/Migrations/20201112163555_V16_Add_CO2_dataset.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Purefolio_backend.Migrations
+{
+    public partial class V16_Add_CO2_dataset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "co2",
+                table: "NaceRegionData",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "co2",
+                table: "NaceRegionData");
+        }
+    }
+}
